8b5c56df2162b697623cace49ef73fd748bf1a24
Коноваленко Виталий Вадимович <vivakon@komus.lan>
2026-05-16 17:04:23 +0300

    first commit

---
+++ 437_summ_way3.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    public:
+        int pathSum(TreeNode* root, int targetSum) {
+            unordered_map<long, int> prefixSum;
+            prefixSum[0] = 1;
+            return dfs(root, 0, targetSum, prefixSum);
+        }
+    
+    private:
+        int dfs(TreeNode* node, long currentSum, int targetSum, unordered_map<long, int>& prefixSum) {
+            if (!node) return 0;
+    
+            currentSum += node->val;
+            int count = prefixSum[currentSum - targetSum];
+    
+            prefixSum[currentSum]++;
+            count += dfs(node->left, currentSum, targetSum, prefixSum);
+            count += dfs(node->right, currentSum, targetSum, prefixSum);
+            prefixSum[currentSum]--;
+    
+            return count;
+        
+        }
+    };
